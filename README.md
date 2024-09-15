1f63427f3392a92f10a390f4b38e38fc149d88b8
Mehmet Altuğ Akgül <mehmetaltugakgul@Mehmet-MacBook-Pro.local>
2024-09-15 12:42:24 +0300

    updated

---
+++ README.md
@@ -0,0 +1,55 @@
+# KeyForge: Strong Password Generator
+
+KeyForge is a desktop application designed to create strong and customizable passwords.
+
+## Features
+
+- Adjustable password length (4-128 characters)
+- Options for uppercase letters, lowercase letters, numbers, and special characters
+- Option to include at least one of each selected character type
+- Password strength indicator
+- Copy generated password to clipboard
+- System tray support
+
+## Installation
+
+1. Download and install Go from the [official website](https://golang.org/).
+
+2. Install the Fyne library:
+   ```
+   go get fyne.io/fyne/v2
+   ```
+3. Clone this project:
+   ```
+   git clone https://github.com/mehmetaltugakgul/keyforge.git
+   ```
+4. Navigate to the project directory:
+   ```
+   cd keyforge
+   ```
+5. Compile and run the application:
+   ```
+   go run main.go
+   ```
+
+## Usage
+
+1. Launch the application.
+2. Adjust the desired password length using the slider.
+3. Select the character types you want to include in your password.
+4. Check the "At Least One of Each Selected Type" option to ensure the password contains at least one character from each selected type.
+5. Click the "Generate Password" button to create your password.
+6. View the generated password and check its strength.
+7. Use the "Copy Password" button to copy the password to your clipboard.
+
+## Building
+
+To build the application for your current platform:
+
+## Contributing
+
+If you'd like to contribute to the project, please fork the repository and create a Pull Request. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.go
@@ -0,0 +1,337 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"unicode"
+
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/widget"
+)
+
+
+var (
+	includeUppercase    bool = true
+	includeLowercase    bool = true
+	includeNumbers      bool = true
+	includeSpecialChars bool = true
+	passwordLength      int  = 12
+	minOneOfEach        bool = false
+	generateButton      *widget.Button
+)
+
+
+
+func atLeastOneSelected() bool {
+	return includeUppercase || includeLowercase || includeNumbers || includeSpecialChars
+}
+
+
+func updateGenerateButtonState(generateButton *widget.Button) {
+	generateButton.Disable()
+	if atLeastOneSelected() {
+		generateButton.Enable()
+	}
+}
+
+func generatePassword(length int, uppercase, lowercase, numbers, specialChars, minOneOfEach bool) string {
+	if !uppercase && !lowercase && !numbers && !specialChars {
+		return ""
+	}
+
+	uppercasePool := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowercasePool := "abcdefghijklmnopqrstuvwxyz"
+	numbersPool := "0123456789"
+	specialPool := "!@#$%^&*()-_+="
+
+	characterSet := ""
+	if uppercase {
+		characterSet += uppercasePool
+	}
+	if lowercase {
+		characterSet += lowercasePool
+	}
+	if numbers {
+		characterSet += numbersPool
+	}
+	if specialChars {
+		characterSet += specialPool
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	password := make([]byte, length)
+
+	if minOneOfEach {
+		requiredTypes := 0
+		if uppercase {
+			requiredTypes++
+		}
+		if lowercase {
+			requiredTypes++
+		}
+		if numbers {
+			requiredTypes++
+		}
+		if specialChars {
+			requiredTypes++
+		}
+
+		if length < requiredTypes {
+			
+			minOneOfEach = false
+		} else {
+			remainingLength := length - requiredTypes
+			currentIndex := 0
+
+			if uppercase {
+				password[currentIndex] = uppercasePool[rand.Intn(len(uppercasePool))]
+				currentIndex++
+			}
+			if lowercase {
+				password[currentIndex] = lowercasePool[rand.Intn(len(lowercasePool))]
+				currentIndex++
+			}
+			if numbers {
+				password[currentIndex] = numbersPool[rand.Intn(len(numbersPool))]
+				currentIndex++
+			}
+			if specialChars {
+				password[currentIndex] = specialPool[rand.Intn(len(specialPool))]
+				currentIndex++
+			}
+
+			
+			for i := 0; i < remainingLength; i++ {
+				password[currentIndex] = characterSet[rand.Intn(len(characterSet))]
+				currentIndex++
+			}
+
+		
+			rand.Shuffle(len(password), func(i, j int) {
+				password[i], password[j] = password[j], password[i]
+			})
+		}
+	} else {
+		
+		for i := range password {
+			password[i] = characterSet[rand.Intn(len(characterSet))]
+		}
+	}
+
+	return string(password)
+}
+
+func calculatePasswordStrength(password string) float64 {
+	var score float64
+	length := len(password)
+
+	if length > 8 {
+		score += float64(length) * 0.5
+	}
+
+	hasUpper := false
+	hasLower := false
+	hasNumber := false
+	hasSpecial := false
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	if hasUpper {
+		score += 10
+	}
+	if hasLower {
+		score += 10
+	}
+	if hasNumber {
+		score += 10
+	}
+	if hasSpecial {
+		score += 15
+	}
+
+
+	varietyCount := 0
+	if hasUpper {
+		varietyCount++
+	}
+	if hasLower {
+		varietyCount++
+	}
+	if hasNumber {
+		varietyCount++
+	}
+	if hasSpecial {
+		varietyCount++
+	}
+	score += float64(varietyCount) * 10
+
+	normalizedScore := score / 100
+
+	if normalizedScore > 1 {
+		normalizedScore = 1
+	}
+
+	return normalizedScore
+}
+
+func main() {
+
+	myApp := app.New()
+	myWindow := myApp.NewWindow("KeyForge")
+
+	myWindow.Hide()
+
+	clipboard := myWindow.Clipboard()
+
+	icon, err := fyne.LoadResourceFromPath("/Users/mehmetaltugakgul/Desktop/password/passwd.png")
+	if err != nil {
+		fmt.Println("Error loading icon:", err)
+	}
+
+	if desk, ok := myApp.(desktop.App); ok {
+		menu := fyne.NewMenu("Password Generator",
+			fyne.NewMenuItem("Show", func() {
+				myWindow.Show()
+			}),
+			
+		)
+		desk.SetSystemTrayMenu(menu)
+		if icon != nil {
+			desk.SetSystemTrayIcon(icon)
+		}
+	} else {
+		fmt.Println("Desktop features not supported")
+	}
+
+
+	lengthSlider := widget.NewSlider(4, 128)
+	lengthSlider.SetValue(12)
+	lengthSlider.Step = 1
+	
+	lengthLabel := widget.NewLabel("Password Length: 12")
+	lengthSlider.OnChanged = func(value float64) {
+		passwordLength = int(value)
+		lengthLabel.SetText(fmt.Sprintf("Password Length: %d", passwordLength))
+	}
+
+	lettersCheck := widget.NewCheck("Include Letters", func(checked bool) {
+		
+		
+	})
+	lettersCheck.SetChecked(true)
+
+	specialCharsCheck := widget.NewCheck("Include Special Characters", func(checked bool) {
+		includeSpecialChars = checked
+	})
+	specialCharsCheck.SetChecked(true)
+
+	uppercaseCheck := widget.NewCheck("Include Uppercase", func(checked bool) {
+		includeUppercase = checked
+		if generateButton != nil {
+			updateGenerateButtonState(generateButton)
+		}
+	})
+	uppercaseCheck.SetChecked(includeUppercase)
+
+	lowercaseCheck := widget.NewCheck("Include Lowercase", func(checked bool) {
+		includeLowercase = checked
+		if generateButton != nil {
+			updateGenerateButtonState(generateButton)
+		}
+	})
+	lowercaseCheck.SetChecked(includeLowercase)
+
+	numbersCheck := widget.NewCheck("Include Numbers", func(checked bool) {
+		includeNumbers = checked
+		if generateButton != nil {
+			updateGenerateButtonState(generateButton)
+		}
+	})
+	numbersCheck.SetChecked(includeNumbers)
+
+	minOneOfEachCheck := widget.NewCheck("At Least One of Each Selected Type", func(checked bool) {
+		minOneOfEach = checked
+	})
+
+	strengthProgress := widget.NewProgressBar()
+	strengthProgress.Hide()
+
+	
+	strengthLabel := widget.NewLabel("")
+	strengthLabel.Hide()
+
+	passwordOutput := widget.NewEntry()
+	passwordOutput.Disable()
+
+
+	generateButton = widget.NewButton("Generate Password", func() {
+		password := generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSpecialChars, minOneOfEach)
+		passwordOutput.SetText(password)
+		
+		strength := calculatePasswordStrength(password)
+		strengthProgress.SetValue(strength)
+		strengthLabel.SetText(fmt.Sprintf("Password Strength: %.0f%%", strength*100))
+	
+		strengthProgress.Show()
+		
+	
+	})
+
+
+	copyButton := widget.NewButton("Copy Password", func() {
+		clipboard.SetContent(passwordOutput.Text)
+	})
+
+
+	exitButton := widget.NewButton("Exit", func() {
+		myApp.Quit()
+	})
+
+
+	content := container.NewVBox(
+		lengthLabel,
+		lengthSlider,
+		uppercaseCheck,
+		lowercaseCheck,
+		numbersCheck,
+		specialCharsCheck,
+		minOneOfEachCheck,
+		generateButton,
+		widget.NewLabel("Generated Password:"),
+		passwordOutput,
+		strengthProgress,
+		strengthLabel,
+		copyButton,
+		exitButton,
+	)
+
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(300, 300))
+
+	myWindow.SetFixedSize(true)
+	myWindow.CenterOnScreen()
+
+	
+	myWindow.SetCloseIntercept(func() {
+		myWindow.Hide()
+	})
+
+	
+	myApp.Run()
+}
